1fbbed3a23a05c0c40f6450d5cf02b362b9f588b
Jeremy D. Frens <jdfrens@gmail.com>
2020-08-28 14:55:40 -0500

    [issue-3] add basic Tetrad definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
+#[derive(Debug, PartialEq)]
+pub struct Tetrad {
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
+}
+
+impl Tetrad {
+    fn new(x: f64, y: f64, z: f64, w: f64) -> Tetrad {
+        Tetrad { x, y, z, w }
+    }
+
+    pub fn point(x: f64, y: f64, z: f64) -> Tetrad {
+        Tetrad::new(x, y, z, 1.0)
+    }
+
+    pub fn vector(x: f64, y: f64, z: f64) -> Tetrad {
+        Tetrad::new(x, y, z, 0.0)
+    }
+
+    pub fn is_point(&self) -> bool {
+        (self.w - 1.0).abs() < f64::EPSILON
+    }
+
+    pub fn is_vector(&self) -> bool {
+        (self.w - 0.0).abs() < f64::EPSILON
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_point() {
+        let tetrad = Tetrad::point(1.0, 2.0, 3.0);
+        assert_eq!(tetrad.is_point(), true);
+        assert_eq!(tetrad.is_vector(), false);
+    }
+
+    #[test]
+    fn test_vector() {
+        let tetrad = Tetrad::vector(1.0, 2.0, 3.0);
+        assert_eq!(tetrad.is_vector(), true);
+        assert_eq!(tetrad.is_point(), false);
     }
 }
